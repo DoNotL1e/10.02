da54ed1fbebc389be2beea7cb753435a3c5c71cc
Jordanas <jordnanaas12@gmail.com>
2024-10-02 20:38:34 +0300

    Initial commit

--- app/src/main/java/com/example/a1002/MainActivity.java
+++ app/src/main/java/com/example/a1002/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a1002;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text1;
+    Button BchnTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ChangeText), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        BchnTxt = findViewById(R.id.BchnTxt);
+        text1 = findViewById(R.id.text1);
+        BchnTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text1.setText("Labas Pasauli!");
+            }
         });
     }
 }
